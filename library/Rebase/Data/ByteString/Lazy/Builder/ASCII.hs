db7dc67e6a27f440e61beaac830b29a357dcbc3c
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-03-28 13:41:15 +0300

    Ditch the lazy builder modules

--- library/Rebase/Data/ByteString/Lazy/Builder/ASCII.hs
+++
@@ -1,7 +0,0 @@
-module Rebase.Data.ByteString.Lazy.Builder.ASCII
-(
-  module Data.ByteString.Lazy.Builder.ASCII
-)
-where
-
-import Data.ByteString.Lazy.Builder.ASCII
--- library/Rebase/Data/ByteString/Lazy/Builder/Extras.hs
+++
@@ -1,7 +0,0 @@
-module Rebase.Data.ByteString.Lazy.Builder.Extras
-(
-  module Data.ByteString.Lazy.Builder.Extras
-)
-where
-
-import Data.ByteString.Lazy.Builder.Extras
